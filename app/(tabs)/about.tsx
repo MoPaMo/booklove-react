522eaf7c22b5a3c8618a60bd5beeae45a84d5c6e
MoPaMo <mopamo@users.noreply.github.com>
2024-12-10 17:24:02 +0100

    Add background image and update styles in About screen

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,21 +1,35 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from "react-native";
+import { Image } from "expo-image";
+
+const PlaceholderImage = require("@/assets/images/background.jpg");
 
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={styles.text}>About screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Arial",
+    color: "#fff",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
   },
-  text: {
-    color: '#fff',
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
   },
 });

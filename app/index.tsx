d27dc9d974229b7fb007762dc3512e1c2e237a55
MoPaMo <mopamo@users.noreply.github.com>
2024-12-10 17:02:38 +0100

    Add navigation and about screen to the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import {Link} from "expo-router"
 
 export default function Index() {
   return (
@@ -6,6 +7,7 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>About</Link>
     </View>
   );
 }
@@ -20,4 +22,9 @@ const styles = StyleSheet.create({
     text: {
       color: '#fff',
     },
+    button: {
+        fontSize: 20,
+        textDecorationLine: 'underline',
+        color: '#fff',
+      },
   });

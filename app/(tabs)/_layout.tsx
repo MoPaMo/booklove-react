db51910c39d92864d6fc126609a1f5a727fe44e9
MoPaMo <mopamo@users.noreply.github.com>
2024-12-11 10:07:02 +0100

    Add Feed and Quotes tabs; update tab icons and layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import { Tabs } from "expo-router";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function TabLayout() {
   return (
@@ -12,6 +11,8 @@ export default function TabLayout() {
         headerStyle: {
           backgroundColor: "#25292e",
         },
+        headerShown: false,
+        
         headerShadowVisible: false,
         headerTintColor: "#fff",
         tabBarStyle: {
@@ -20,28 +21,14 @@ export default function TabLayout() {
         },
       }}
     >
+
       <Tabs.Screen
-        name="index"
+        name="feed"
         options={{
-          title: "Home",
+          title: "Feed",
           tabBarIcon: ({ color, focused }) => (
             <Ionicons
-              name={focused ? "home-sharp" : "home-outline"}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? "information-circle" : "information-circle-outline"
-              }
+              name="newspaper" 
               color={color}
               size={24}
             />
@@ -54,23 +41,36 @@ export default function TabLayout() {
         options={{
           title: "Quotes",
           tabBarIcon: ({ color, focused }) => (
-            <MaterialCommunityIcons name="comment-quote" size={24} color={color} />
+            <MaterialCommunityIcons
+              name="comment-quote"
+              size={24}
+              color={color}
+            />
           ),
         }}
       />
       <Tabs.Screen
-      name="account"
+        name="about"
         options={{
-            title: "Account",
-            tabBarIcon: ({ color, focused }) => (
+          title: "List",
+          tabBarIcon: ({ color, focused }) => (
             <Ionicons
-                name={focused ? "person-circle" : "person-circle-outline"}
-                color={color}
-                size={24}
+              name="library"
+              color={color}
+              size={24}
             />
-            ),
+          ),
         }}
-        />
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name="person-circle-outline" color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }

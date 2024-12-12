2f9d577da6b261910eabf83709210ed31f76965b
MoPaMo <mopamo@users.noreply.github.com>
2024-12-12 13:23:20 +0100

    Implement automatic redirection to Feed screen on Home screen load

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,42 @@
-import { Text, View, StyleSheet } from "react-native";
-import {Link} from "expo-router"
+import { View, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
+import { useEffect } from "react";
+import { useRouter } from "expo-router";
 
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const redirect = async () => {
+      router.push("/feed");
+    };
+
+    redirect();
+  }, []);
+
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>About</Link>
+      <Link href="/about" style={styles.button}>
+        About
+      </Link>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#25292e',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      color: '#fff',
-    },
-    button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#fff',
-      },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    color: "#fff",
+    marginTop: 20,
+  },
+});

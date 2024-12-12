a8d0cf7361332f2470eae4924e5727527997f52d
MoPaMo <mopamo@users.noreply.github.com>
2024-12-12 22:24:27 +0100

    Add additional PlayfairDisplay font weights for improved typography in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Stack } from "expo-router";
 import { View, ActivityIndicator } from "react-native";
 import { useFonts } from "expo-font";
-import { PlayfairDisplay_900Black } from "@expo-google-fonts/dev";
+import { PlayfairDisplay_900Black, PlayfairDisplay_400Regular, PlayfairDisplay_700Bold } from "@expo-google-fonts/dev";
 
 export default function RootLayout() {
   let [fontsLoaded] = useFonts({
-    PlayfairDisplay_900Black,
+    PlayfairDisplay_900Black,PlayfairDisplay_700Bold,PlayfairDisplay_400Regular
   });
 
   if (!fontsLoaded) {

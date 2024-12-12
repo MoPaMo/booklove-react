5cc48c898a30f0dcb7d0f34410b1bf4ff33e2c06
MoPaMo <mopamo@users.noreply.github.com>
2024-12-12 17:09:15 +0100

    Update AboutScreen layout to adjust topSection padding based on screen height for improved responsiveness

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,8 +1,9 @@
 import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
 import Container from "@/components/DefaultBGProvider";
 import colors from "@/utils/colors";
+import { Dimensions } from "react-native";
 
-
+let { width, height } = Dimensions.get("window");
 export default function AboutScreen() {
   return (
     <Container>
@@ -14,24 +15,23 @@ export default function AboutScreen() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      margin: 16,
-    },
-  
-    topSection: {
-        paddingTop: 0, 
-      justifyContent: "flex-end",
-      paddingBottom: 10, // Add padding if needed
-    },
-  
-    header: {
-      flexWrap: "wrap",
-      fontSize: 64, 
-      color: colors.orange,
-      textAlign: "left",
-      fontFamily: "PlayfairDisplay_900Black",
-      lineHeight: 64,
-      flex: 1,
-    },
-  });
+  container: {
+    flex: 1,
+    margin: 16,
+  },
+
+  topSection: {
+    paddingTop: height * 0.2,
+    justifyContent: "flex-end",
+    paddingBottom: 10, // Add padding if needed
+  },
+  header: {
+    flexWrap: "wrap",
+    fontSize: 64,
+    color: colors.orange,
+    textAlign: "left",
+    fontFamily: "PlayfairDisplay_900Black",
+    lineHeight: 64,
+    flex: 1,
+  },
+});

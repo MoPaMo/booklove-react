5c733e1671980b0576029bd4d8e983bd45bea80b
MoPaMo <mopamo@users.noreply.github.com>
2024-12-15 17:18:56 +0100

    feat: enhance Text component with customizable font styles

--- components/utils/text.tsx
+++ components/utils/text.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { Text as RNText, StyleSheet } from "react-native"; // rename to avoid conflict with custom
 import colors from "@/utils/colors";
-const Text = (props: any) => {
+
+interface TextProps {
+  fontStyle?: "serif" | "sansSerif" | "mono";
+  style?: any;
+  children: React.ReactNode;
+}
+
+const Text = (props: TextProps) => {
+  const { fontStyle = "sansSerif", style, children, ...rest } = props;
+
+  const fontStyles = {
+    serif: { fontFamily: "PlayfairDisplay_400Regular" },
+    sansSerif: { fontFamily: "Raleway_400Regular" },
+    mono: { fontFamily: "RobotoMono_400Regular" },
+  };
+
   return (
-    <RNText {...props} style={[styles.text, props.style]}>
-      {props.children}
+    <RNText
+      {...rest}
+      style={[styles.text, fontStyle && fontStyles[fontStyle], style]}
+    >
+      {children}
     </RNText>
   );
 };

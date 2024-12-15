ca7a06d834aca45c084dd0082c07f3a62c05a56c
MoPaMo <mopamo@users.noreply.github.com>
2024-12-15 21:17:20 +0100

    feat: add link to book in Account screen

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Container from '@/components/DefaultBGProvider';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+import Container from "@/components/DefaultBGProvider";
 const Account = () => {
-    return (
-        <Container>
-            <Text style={styles.text}>This is the Account screen</Text>
-        </Container>
-    );
+  return (
+    <Container>
+      <Text style={styles.text}>This is the Account screen</Text>
+      <Link href="/book/a">a book</Link>
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-    },
-    text: {
-        fontSize: 18,
-        color: '#333',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 18,
+    color: "#333",
+  },
 });
 
 export default Account;

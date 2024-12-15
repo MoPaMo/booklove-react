26f0e0b6149e2e046bc4fece988447c2cea4b092
MoPaMo <mopamo@users.noreply.github.com>
2024-12-15 16:41:11 +0100

    feat: add custom Text component with default styles for typography

---
+++ components/utils/text.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text as RNText, StyleSheet } from "react-native";
+import colors from "@/utils/colors";
+const Text = (props: any) => {
+  return (
+    <RNText {...props} style={[styles.text, props.style]}>
+      {props.children}
+    </RNText>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: "Raleway_400Regular",
+    fontSize: 16,
+    color: colors.primaryText,
+  },
+});
+
+export default Text;

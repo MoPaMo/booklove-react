0417be04f8f218be4d2e4f12621bc47e155afb08
MoPaMo <mopamo@users.noreply.github.com>
2024-12-15 16:32:15 +0100

    feat: add utility styles for typography and color management

---
+++ utils/utils.ts
@@ -0,0 +1,17 @@
+export const styles = {
+  mono: {
+    fontFamily: "RobotoMono_400Regular",
+  },
+  sansSerif: {
+    fontFamily: "Raleway_300Light",
+  },
+  primaryTextColor: {
+    color: "#333333",
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  regularText: {
+    fontWeight: "normal",
+  },
+};

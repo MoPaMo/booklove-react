b3024e846ecc078d2372aee10979a3bb71b1cfa5
MoPaMo <mopamo@users.noreply.github.com>
2024-12-27 14:12:22 +0100

    feat: refactor AboutScreen component to use styled-components for improved styling

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,22 +1,28 @@
-import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
+import { Text, View, SafeAreaView, ScrollView } from "react-native";
+import styled from "styled-components/native";
 import Container from "@/components/DefaultBGProvider";
 import colors from "@/utils/colors";
 import { Dimensions } from "react-native";
 import FeedbackComponent from "@/components/feed/FeedbackComponent";
 import HeaderTitle from "@/components/headerTitle";
 let { width, height } = Dimensions.get("window");
-export default function AboutScreen() {
-  return (
-    <Container>
-      <HeaderTitle color={colors.orange}>Your Books</HeaderTitle>
-      <FeedbackComponent />
-    </Container>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 16,
-  },
-});
+const Header = styled(HeaderTitle)`
+  color: ${(props) => props.theme.orange};
+`;
+
+const ContainerStyled = styled(Container)`
+  flex: 1;
+  padding: 16px;
+`;
+
+const AboutScreen = () => {
+  return (
+    <ContainerStyled>
+      <Header>Your Books</Header>
+      <FeedbackComponent />
+    </ContainerStyled>
+  );
+};
+
+export default AboutScreen;

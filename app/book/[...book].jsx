7987c9ca9d90420936b68759d7bf8af74301a205
MoPaMo <mopamo@users.noreply.github.com>
2024-12-28 16:10:01 +0100

    refactor: enhance Book component with layout animation and expandable description

--- app/book/[...book].jsx
+++ app/book/[...book].jsx
@@ -8,7 +8,21 @@ import Text from "@/components/utils/text";
 import UserStack from "@/components/userStack";
 import styled, { useTheme } from "styled-components/native";
 import { useNavigation, useRoute } from "@react-navigation/native";
-import { TouchableOpacity } from "react-native";
+import {
+  TouchableOpacity,
+  LayoutAnimation,
+  Platform,
+  UIManager,
+} from "react-native";
+
+// Enable LayoutAnimation on Android
+if (
+  Platform.OS === "android" &&
+  UIManager.setLayoutAnimationEnabledExperimental
+) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
+
 const StyledContainer = styled(Container)`
   flex: 1;
 `;
@@ -33,9 +47,15 @@ const Description = styled(Text)`
   font-family: "PlayfairDisplay_400Regular";
   font-size: 16px;
   color: ${(props) => props.theme.primaryText};
-  max-height: ${(props) => (props.isExpanded ? '500px' : '100px')};
+  max-height: ${(props) => (props.isExpanded ? "500px" : "100px")};
+  background-color: ${(props) =>
+    props.isExpanded ? props.theme.CleanBG + "5f" : "transparent"};
+  padding: ${(props) => (props.isExpanded ? "5px" : "0px")};
+  margin-bottom: ${(props) => (props.isExpanded ? "5px" : "0px")};
+  border-radius: 10px;
   overflow: hidden;
   transition: max-height 0.3s ease;
+
 `;
 
 const MoreText = styled(Text)`
@@ -77,21 +97,28 @@ const Book = () => {
   const navigation = useNavigation();
   const route = useRoute();
   const { book } = route.params || {};
-  const text = "Mr Bennet, owner of the Longbourn estate in Hertfordshire, has five daughters, but his property is entailed and can only be passed to a male heir. His wife also lacks an inheritance, so his family faces...";
-  useLayoutEffect(() => { 
+  const text =
+    'Mr Bennet, owner of the Longbourn estate in Hertfordshire, has five daughters, but his property is entailed and can only be passed to a male heir. His wife also lacks an inheritance, so his family faces becoming poor upon his death. Thus, it is imperative that at least one of the daughters marry well to support the others, which is a primary motivation driving the plot. Pride and Prejudice has consistently appeared near the top of lists of "most-loved books" among literary scholars and the reading public. It has become one of the most popular novels in English literature, with over 20 million copies sold, and has inspired many derivatives in modern literature.[1][2] For more than a century, dramatic adaptations, reprints, unofficial sequels, films, and TV versions of Pride and Prejudice have portrayed the memorable characters and themes of the novel, reaching mass audiences';
+
+  useLayoutEffect(() => {
     navigation.setOptions({
       title: book?.title || "Book Details",
       headerBackTitle: "Back",
     });
   }, [navigation, book]);
 
+  const toggleExpansion = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <StyledContainer>
       <HeaderTitle color={color.cyan}>Pride and Prejudice</HeaderTitle>
       <AuthorText>Jane Austen, 1813</AuthorText>
       <Divider />
-      <Description isExpanded={isExpanded} onPress={() => setIsExpanded(!isExpanded)}>
-        {isExpanded ? text : `${text.substring(0, 100)}...`}
+      <Description isExpanded={isExpanded} onPress={toggleExpansion}>
+        {isExpanded ? text+" " : `${text.substring(0, 100)}... `}
         <MoreText>{isExpanded ? " less" : " more"}</MoreText>
       </Description>
 

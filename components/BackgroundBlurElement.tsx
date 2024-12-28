5a2c238a8ba84c8a60cb012c898eb24fe0176015
MoPaMo <mopamo@users.noreply.github.com>
2024-12-28 09:40:40 +0100

    feat: refactor BackgroundBlurElement component to use styled-components for improved styling

--- components/BackgroundBlurElement.tsx
+++ components/BackgroundBlurElement.tsx
@@ -1,48 +1,44 @@
 import React from "react";
-import { View, Image, StyleSheet, Dimensions } from "react-native";
+import { Dimensions } from "react-native";
 import { BlurView } from "expo-blur";
+import styled from "styled-components/native";
 import colors from "@/utils/colors";
 const { width, height } = Dimensions.get("window");
 
+const Container = styled.View`
+  ${StyleSheet.absoluteFillObject}
+  z-index: 0;
+  flex: 1;
+  background-color: ${(props) => props.theme.CleanBG};
+  color: ${(props) => props.theme.primaryText};
+`;
+
+const OriginalBG1 = styled.Image`
+  position: absolute;
+  width: ${(props) => props.width * 0.61}px;
+  top: 0;
+  right: ${(props) => props.width * -0.1}px;
+`;
+
+const OriginalBG2 = styled.Image`
+  position: absolute;
+  width: ${(props) => props.width * 0.79}px;
+  left: ${(props) => props.width * -0.2}px;
+  bottom: 28px;
+`;
+
+const BlurredView = styled(BlurView)`
+  ${StyleSheet.absoluteFill}
+`;
+
 const BackgroundBlurElement = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require("@/assets/images/backgrounds/blur3.png")}
-        style={[styles.image, styles.originalBG1]}
-      />
-      <Image
-        source={require("@/assets/images/backgrounds/blur_hex.png")}
-        style={[styles.image, styles.originalBG2]}
-      />
-      <BlurView intensity={8} style={StyleSheet.absoluteFill} />
-    </View>
+    <Container>
+      <OriginalBG1 source={require("@/assets/images/backgrounds/blur3.png")} />
+      <OriginalBG2 source={require("@/assets/images/backgrounds/blur_hex.png")} />
+      <BlurredView intensity={8} />
+    </Container>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    zIndex: 0,
-    flex: 1,
-    backgroundColor: colors.CleanBG,
-    color: colors.primaryText,
-  },
-  originalBG1: {
-    position: "absolute",
-    width: width * 0.61,
-    top: 0,
-    right: width * -0.1,
-  },
-  originalBG2: {
-    position: "absolute",
-    width: width * 0.79,
-    left: width * -0.2,
-    bottom: 28,
-  },
-  image: {
-    resizeMode: "contain",//width > 700 ? "contain" : "cover",
-  },
-});
 
 export default BackgroundBlurElement;

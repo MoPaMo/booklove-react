98d7918ba6c8152ba603c75717aa43e565ef857d
MoPaMo <mopamo@users.noreply.github.com>
2024-12-28 15:41:53 +0100

    refactor: integrate HeaderTitle component and apply theme color in Account screen

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,7 +1,9 @@
-import styled from "styled-components/native";
+import styled, {useTheme} from "styled-components/native";
 import {Text} from "react-native";
 import Container from "@/components/DefaultBGProvider";
 import { Link } from 'expo-router';
+import HeaderTitle from "@/components/headerTitle";
+
 const ContainerStyled = styled(Container)`
   flex: 1;
   justify-content: center;
@@ -9,15 +11,18 @@ const ContainerStyled = styled(Container)`
   background-color: ${(props) => props.theme.CleanBG};
 `;
 
+
+
 const TextStyled = styled(Text)`
   font-size: 18px;
   color: ${(props) => props.theme.primaryText};
 `; 
 
 const Account = () => {
+    const color = useTheme();
   return (
     <ContainerStyled>
-      <TextStyled>This is the Account screen</TextStyled>
+        <HeaderTitle color={color.green}>Your Account</HeaderTitle>
       <Link href="/book/a">a book</Link>
     </ContainerStyled>
   ); 
